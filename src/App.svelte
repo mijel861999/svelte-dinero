3d656252332d8b0d16dd80425e95ae46d26f2442
mijel <mijel.dev@gmail.com>
2022-11-05 12:07:02 -0500

    Primer commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let nombre = "Miguel";
+  import Boton from "./components/Boton.svelte";
+</script>
+
+<main>
+  <input type="text" bind:value={nombre} />
+  <h1>Hola, {nombre}!</h1>
+  <Boton />
+</main>
+
+<style>
+  h1 {
+    color: green;
+  }
+</style>
---
+++ src/components/Boton.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<div class="button">
+  <button on:click={handleClick}>Click me: {count}</button>
+</div>
+
+<style></style>
